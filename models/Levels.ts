525f32796170124991fc76bfa1b3208c81a3f6bd
Voakie <contact@voakie.com>
2022-12-15 14:56:39 +0100

    Add hard level (broken)

--- models/Levels.ts
+++ models/Levels.ts
@@ -1,6 +1,7 @@
 import { GameLevel } from "../components/GameState"
 import createLevelEasy1 from "./LevelEasy1"
 import createLevelEasy2 from "./LevelEasy2"
+import createLevelHard1 from "./LevelHard1"
 import createLevelMedium1 from "./LevelMedium1"
 import createLevelMedium2 from "./LeveMedium2"
 import createStartingScreenLevel from "./StartingScreenLevel"
@@ -12,6 +13,7 @@ class Levels {
         createLevelEasy2(),
         createLevelMedium1(),
         createLevelMedium2(),
+        // createLevelHard1(),
     ]
 
     getStartingLevel() {
